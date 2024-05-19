59474a33281ba75c9ef033e6f0284a384b3b238b
Przemek <przemyslaw.makowka@o2.pl>
2024-05-19 14:37:51 +0200

    Revert "Merge pull request #3 from infoshareacademy/inga/JPYDZR5RFD-24/przygotowanie_pliku_csv"
    This reverts commit 1f80dc63e3ba231306df004b1b0da55759c41d77, reversing
    changes made to 3263680275f8b09b6ac74c330c5896d5f8ba5004.

--- System rezerwacji biletów/main.py
+++
@@ -1,9 +0,0 @@
-from Menu import Menu
-
-if __name__ == '__main__':
-    menu = Menu()
-    print(menu)
-    option = None
-    while option != '5':
-        option = menu.choose_option()
-        print(menu)
