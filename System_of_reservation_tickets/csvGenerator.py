4f65d6c01a1d365762f4daf8d497f97683a65772
Inga Piotrowicz <163149022+inga-pi@users.noreply.github.com>
2024-06-09 13:04:47 +0200

    Generowanie i sortowanie dat w pliku csv w przedziale tygodniowym (#17)
    * Generowanie i sortowanie dat w pliku csv w przedziale tygodniowym
    
    * zmiana ścieżki wygenerowanego pliku csv

--- System_of_reservation_tickets/csvGenerator.py
+++ System_of_reservation_tickets/csvGenerator.py
@@ -1,21 +1,31 @@
 import csv
 import random
+from datetime import datetime, timedelta
 
 # Przykładowe dane filmów, dni i godzin seansów
 film_titles = ["Avengers: Endgame", "The Godfather", "Inception", "The Shawshank Redemption", "The Dark Knight",
-                 "Pulp Fiction", "Forrest Gump", "The Matrix", "Schindler's List", "Titanic"]
-show_dates = ["2024-05-15", "2024-05-16", "2024-05-17", "2024-05-18", "2024-05-19"]
+               "Pulp Fiction", "Forrest Gump", "The Matrix", "Schindler's List", "Titanic"]
 show_hours = ["10:00", "13:30", "16:45", "19:30", "22:15"]
 hall_numbers = ["1", "2", "3", "4", "5"]
 ticket_prices = ["15.00", "18.00", "20.00", "22.00", "25.00"]
 rows = ["A", "B", "C", "D", "E"]
 seats = [str(i) for i in range(1, 11)]
 
+
+# Generowanie dat seansów w przedziale jednego tygodnia od dziś
+def generate_show_dates(days_number=7):
+    today = datetime.today()
+    show_dates = [(today + timedelta(days=i)).strftime("%Y-%m-%d") for i in range(days_number)]
+    return show_dates
+
+
+generated_show_dates = generate_show_dates()
+
 # Generowanie 30 przykładowych danych seansów
 cinema_shows = [["Film_title", "Show_date", "Show_hour", "Hall_number", "Ticket_price", "Row", "Seat"]]
 for _ in range(30):
     film_title = random.choice(film_titles)
-    show_date = random.choice(show_dates)
+    show_date = random.choice(generated_show_dates)
     show_hour = random.choice(show_hours)
     hall_number = random.choice(hall_numbers)
     ticket_price = random.choice(ticket_prices)
@@ -23,8 +33,13 @@ for _ in range(30):
     seat = random.choice(seats)
     cinema_shows.append([film_title, show_date, show_hour, hall_number, ticket_price, row, seat])
 
+
+#Sortowanie danych według daty seansu i godziny seansu
+cinema_shows = [cinema_shows[0]] + sorted(cinema_shows[1:], key=lambda x: (x[1], x[2]))
+
+
 # Ścieżka do pliku CSV
-file_path = "System rezerwacji biletów/data_base.csv"
+file_path = "data_base.csv"
 
 # Zapis do pliku CSV
 try:
