cd88886b7a369d8e930493ddb891efb9db393ded
Przemek <przemyslaw.makowka@o2.pl>
2024-06-14 12:37:12 +0200

    Add exception with info for developer

--- System_of_reservation_tickets/cinema_hall.py
+++ System_of_reservation_tickets/cinema_hall.py
@@ -57,6 +57,10 @@ class CinemaHall:
     def __check_all_seats_are_reserved_or_empty(self, mode: str) -> bool:
         # Dla mode = 'r' sprawdzam czy wszystkie miejsca są zajęte.
         # Dla mode = 'e' sprawdzam czy wszsytkie miejsca są wolne.
+        if mode not in ('r', 'e'):
+            raise ValueError("Parametr mod powinien być równy 'r' lub 'e'."
+                             "Sprawdź wywołanie metody __check_all_seats_are_reserved_or_empty"
+                             " w kodzie!")
         check_seat = 'X' if mode == 'r' else '*'
         count_all_seats_reserved_in_row = 0
         for i in range(self._len_rows):
