824773ccac37d55881614af65761d38d4c5f4d79
Tomasz Brzezina <tomasz@brzezina.pl>
2024-08-11 13:18:08 +0200

    refactoring: renamed folders, added cinema app, joined urls, views and templates, removed unused apps

--- cinema_reservation_system/basket/views.py
+++
@@ -1,8 +0,0 @@
-from django.template.response import TemplateResponse
-
-
-# Create your views here.
-def basket(request):
-    template = "basket/basket.html"
-    message = {"message": "OK!"}
-    return TemplateResponse(request, template, message)
--- cinema_reservation_system/price_list/views.py
+++
@@ -1,8 +0,0 @@
-from django.template.response import TemplateResponse
-
-
-# Create your views here.
-def price_list(request):
-    template = "price_list/price_list.html"
-    message = {"message": "OK!"}
-    return TemplateResponse(request, template, message)
