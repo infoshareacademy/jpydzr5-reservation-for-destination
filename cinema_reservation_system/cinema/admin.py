93eba4291a8329f3bbc036deb59e77a51bc86c79
Inga Piecha <inga.piecha@gmail.com>
2024-08-15 11:52:21 +0200

    skonfigurowanie bazy danych w django

--- cinema_reservation_system/cinema/admin.py
+++ cinema_reservation_system/cinema/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Repertoire, User, Reservation
 
-# Register your models here.
+admin.site.register(Repertoire)
+admin.site.register(User)
+admin.site.register(Reservation)
--- cinema_reservation_system/cinema/models.py
+++ cinema_reservation_system/cinema/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Repertoire(models.Model):
+    movie_title = models.CharField(max_length=100)
+    show_date = models.CharField(max_length=10)
+    show_hour = models.CharField(max_length=5)
+    hall_number = models.IntegerField()
+    movie_description = models.CharField(max_length=300)
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.movie_title
+
+
+class User(models.Model):
+    name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.name} {self.surname}"
+
+
+class Reservation(models.Model):
+    repertoire = models.ForeignKey(Repertoire, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    row = models.CharField(max_length=1)
+    seat = models.IntegerField()
+
+    def __str__(self):
+        return f"Rezerwacja dla {self.user} na {self.repertoire}"
