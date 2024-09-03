2240898ea59e71fd1adf19a66d108a49a6f088cc
Maksym Fedyna <maximusfeduna@gmail.com>
2024-09-03 17:56:58 +0200

    models and admin

--- cinema_reservation_system/cinema/admin.py
+++ cinema_reservation_system/cinema/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from django.contrib import admin
+from .models import Cinema, Room, Seat
+
+admin.site.register(Cinema)
+admin.site.register(Room)
+admin.site.register(Seat)
--- cinema_reservation_system/cinema/models.py
+++ cinema_reservation_system/cinema/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class Cinema(models.Model):
+    """Model reprezentujący kino."""
+    name = models.CharField(max_length=100)  # Nazwa kina
+    city = models.CharField(max_length=100)  # Miasto, w którym znajduje się kino
+
+    def __str__(self):
+        return f"{self.name} w {self.city}"
+
+
+class Room(models.Model):
+    """Model reprezentujący salę kinową."""
+    cinema = models.ForeignKey(Cinema, on_delete=models.CASCADE)  # Powiązanie z modelem Cinema
+    room_number = models.CharField(max_length=10)  # Numer sali
+    cleaning_time = models.DurationField()  # Czas sprzątania
+
+    def __str__(self):
+        return f"Sala {self.room_number} w kinie {self.cinema.name}"
+
+
+class Seat(models.Model):
+    """Model reprezentujący miejsce w sali kinowej."""
+    STATUS_CHOICES = [
+        ('niedostępne', 'Niedostępne'),
+        ('uszkodzone', 'Uszkodzone'),
+        ('dostępne', 'Dostępne'),
+    ]
+
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)  # Powiązanie z modelem Room
+    row = models.CharField(max_length=5)  # Rząd
+    column = models.CharField(max_length=5)  # Kolumna
+    status = models.CharField(max_length=12, choices=STATUS_CHOICES, default='dostępne')  # Stan
+
+    def __str__(self):
+        return f"Miejsce {self.row}-{self.column} w sali {self.room.room_number} ({self.get_status_display()})"
