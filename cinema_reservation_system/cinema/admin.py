41a90138e18bde96165fc0a4cb92ed66cf470394
Inga Piecha <inga.piecha@gmail.com>
2024-09-04 12:04:03 +0200

    dodanie tabeli cennik i przywrocenie usunietych modeli

--- cinema_reservation_system/cinema/admin.py
+++ cinema_reservation_system/cinema/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Repertoire, User, Reservation, Price
 
-# Register your models here.
+admin.site.register(Repertoire)
+admin.site.register(User)
+admin.site.register(Reservation)
+admin.site.register(Price)
