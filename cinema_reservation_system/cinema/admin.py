08cc30f9cd08aceceabbb7d5c589130ddbfb080f
Inga Piotrowicz <163149022+inga-pi@users.noreply.github.com>
2024-09-06 20:30:53 +0200

    Inga/JPYDZR5RFD-62/dodanie tabeli cennik i przywrocenie usunietych modeli (#51)
    * dodanie tabeli cennik i przywrocenie usunietych modeli
    
    * uzycie dekoratora register w pliku admin

--- cinema_reservation_system/cinema/admin.py
+++ cinema_reservation_system/cinema/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Repertoire, User, Reservation, Price
 
-# Register your models here.
+
+@admin.register(Repertoire)
+class RepertoireAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Price)
+class PriceAdmin(admin.ModelAdmin):
+    pass
