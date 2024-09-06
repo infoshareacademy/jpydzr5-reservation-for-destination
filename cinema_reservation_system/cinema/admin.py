135b8918ed64dbc359de1a52c7ed8b6ebe97b7f9
Inga Piecha <inga.piecha@gmail.com>
2024-09-06 20:25:13 +0200

    uzycie dekoratora register w pliku admin

--- cinema_reservation_system/cinema/admin.py
+++ cinema_reservation_system/cinema/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 from .models import Repertoire, User, Reservation, Price
 
-admin.site.register(Repertoire)
-admin.site.register(User)
-admin.site.register(Reservation)
-admin.site.register(Price)
+
+@admin.register(Repertoire)
+class RepertoireAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Price)
+class PriceAdmin(admin.ModelAdmin):
+    pass
