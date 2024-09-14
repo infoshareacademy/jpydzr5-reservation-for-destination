e564f2d56705820a81125f9a725ddd2c1eb71787
Tomasz Brzezina <tomasz@brzezina.pl>
2024-09-15 01:26:43 +0200

    linux fix

--- tools/database_dump.sh
+++ tools/database_dump.sh
@@ -2,7 +2,6 @@
 cd ../cinema_reservation_system
 python manage.py dumpdata cinema.cinema --indent 2 > cinema/fixtures/cinemas.json
 python manage.py dumpdata cinema.hall --indent 2 > cinema/fixtures/halls.json
-echo "test"
 # źle działa pod windowsem
 #python manage.py dumpdata cinema.movie --indent 2 > cinema/fixtures/movies.json
 python manage.py dumpdata cinema.tickettype --indent 2 > cinema/fixtures/ticket_types.json
