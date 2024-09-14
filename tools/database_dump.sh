8c27c3453a424ec1eb75f02f516ba7e51bb14f5d
Tomasz Brzezina <tomasz@brzezina.pl>
2024-09-14 19:48:15 +0200

    cleaning and dumping

--- tools/database_dump.sh
+++ tools/database_dump.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+cd ../cinema_reservation_system
 python manage.py dumpdata cinema.cinema --indent 2 > cinema/fixtures/cinemas.json
 python manage.py dumpdata cinema.hall --indent 2 > cinema/fixtures/halls.json
 python manage.py dumpdata cinema.movie --indent 2 > cinema/fixtures/movies.json
-python manage.py dumpdata cinema.price --indent 2 > cinema/fixtures/prices.json
+python manage.py dumpdata cinema.tickettype --indent 2 > cinema/fixtures/ticket_types.json
+cd ..
--- tools/initial.bat
+++ tools/initial.bat
@@ -2,5 +2,5 @@ cd ../cinema_reservation_system
 python manage.py loaddata cinema/fixtures/cinemas.json
 python manage.py loaddata cinema/fixtures/halls.json
 python manage.py loaddata cinema/fixtures/movies.json
-python manage.py loaddata cinema/fixtures/prices.json
+python manage.py loaddata cinema/fixtures/ticket_types.json
 cd ..
