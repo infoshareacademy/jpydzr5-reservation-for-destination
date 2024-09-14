daf3a729245523ae649955212bdb13049ac46dfe
Tomasz Brzezina <tomasz@brzezina.pl>
2024-09-14 19:41:00 +0200

    more tools

---
+++ tools/database_dump.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+python manage.py dumpdata cinema.cinema --indent 2 > cinema/fixtures/cinemas.json
+python manage.py dumpdata cinema.hall --indent 2 > cinema/fixtures/halls.json
+python manage.py dumpdata cinema.movie --indent 2 > cinema/fixtures/movies.json
+python manage.py dumpdata cinema.price --indent 2 > cinema/fixtures/prices.json
---
+++ tools/initial.bat
@@ -0,0 +1,6 @@
+cd ../cinema_reservation_system
+python manage.py loaddata cinema/fixtures/cinemas.json
+python manage.py loaddata cinema/fixtures/halls.json
+python manage.py loaddata cinema/fixtures/movies.json
+python manage.py loaddata cinema/fixtures/prices.json
+cd ..
