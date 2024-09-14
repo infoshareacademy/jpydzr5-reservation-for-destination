0715cc8574b7d4ec9af44ac533ec31ae1c0c0c88
Tomasz Brzezina <tomasz@brzezina.pl>
2024-09-15 01:29:07 +0200

    TYPO

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -7,8 +7,8 @@ python manage.py loaddata cinema/fixtures/cinemas.json
 python manage.py loaddata cinema/fixtures/halls.json
 python manage.py loaddata cinema/fixtures/movies.json
 python manage.py loaddata cinema/fixtures/ticket_types.json
-python manage.py dumpdata cinema/fixtures/seat_types.json
-python manage.py dumpdata cinema/fixtures/seats.json
-python manage.py dumpdata cinema/fixtures/reservations.json
-python manage.py dumpdata cinema/fixtures/seat_reservations.json
+python manage.py loaddata cinema/fixtures/seat_types.json
+python manage.py loaddata cinema/fixtures/seats.json
+python manage.py loaddata cinema/fixtures/reservations.json
+python manage.py loaddata cinema/fixtures/seat_reservations.json
 cd ..
