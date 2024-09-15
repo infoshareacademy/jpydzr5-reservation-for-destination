8843ae5d25c66298cda4cadac08c31b22010a0bb
Przemek <przemyslaw.makowka@o2.pl>
2024-09-15 14:05:18 +0200

    Change a reservation_json file

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -9,6 +9,7 @@ python manage.py loaddata cinema/fixtures/movies.json
 python manage.py loaddata cinema/fixtures/ticket_types.json
 python manage.py loaddata cinema/fixtures/seat_types.json
 python manage.py loaddata cinema/fixtures/seats.json
+python manage.py loaddata cinema/fixtures/seances.json
 python manage.py loaddata cinema/fixtures/reservations.json
 python manage.py loaddata cinema/fixtures/seat_reservations.json
 cd ..
