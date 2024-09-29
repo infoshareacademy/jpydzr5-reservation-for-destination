302c152a695836247a030724854ecee51175e980
Tomasz Brzezina <tomasz@brzezina.pl>
2024-09-29 19:55:38 +0200

    report in html

--- cinema_reservation_system/backoffice/views.py
+++ cinema_reservation_system/backoffice/views.py
@@ -1,11 +1,20 @@
 from django.db.models import Count
-from django.db.models.functions import ExtractHour, ExtractMinute
+from django.db.models.functions import ExtractHour, ExtractMinute, ExtractWeekDay
 from django.http import HttpResponse
 from django.shortcuts import render
 
 from cinema import models
 import pendulum
 
+DAYS_OF_WEEK = {
+    1: "Niedziela",
+    2: "Poniedziałek",
+    3: "Wtorek",
+    4: "Środa",
+    5: "Czwartek",
+    6: "Piątek",
+    7: "Sobota",
+}
 
 # Create your views here.
 def index(request):
@@ -36,8 +45,33 @@ def html_report(request):
         ).order_by('-total_reserved_seats')
     )
     popular_showtimes = (
-        models.Seance.objects.annotate(hour=ExtractHour('show_start'), minute=ExtractMinute('show_start'))  # Ekstrahuje godzinę
+        models.SeatReservation.objects.annotate(
+            hour=ExtractHour('reservation__seance__show_start'),
+            minute=ExtractMinute('reservation__seance__show_start')
+        ).exclude(hour__isnull=True)
         .values('hour','minute')  # Grupuje po godzinie
+        .annotate(count=Count('id'))  # Zlicza wystąpienia seansów dla każdej godziny
+        .order_by('-count')  # Sortuje według liczby wystąpień malejąco
+    )
+    popular_weekdays = (
+        models.SeatReservation.objects.annotate(weekday=ExtractWeekDay('reservation__seance__show_start'))  # Ekstrahuje godzinę
+        .exclude(weekday__isnull=True)
+        .values('weekday')
+        .annotate(count=Count('id'))  # Zlicza wystąpienia seansów dla każdej godziny
+        .order_by('-count')  # Sortuje według liczby wystąpień malejąco
+    )
+    popular_weekdays = [
+        {
+            'weekday': popular_weekday['weekday'],
+            'day_name': DAYS_OF_WEEK.get(popular_weekday['weekday'], "Nieznany dzień"),
+            'count': popular_weekday['count']
+        }
+        for popular_weekday in popular_weekdays
+    ]
+
+    popular_ticket_types = (
+        models.SeatReservation.objects
+        .values('ticket_type', 'ticket_type__name')
         .annotate(count=Count('id'))  # Zlicza wystąpienia seansów dla każdej godziny
         .order_by('-count')  # Sortuje według liczby wystąpień malejąco
     )
@@ -46,6 +80,8 @@ def html_report(request):
         'range_begin' : range_begin,
         'range_end': range_end,
         'popular_showtimes': popular_showtimes,
+        'popular_weekdays': popular_weekdays,
+        'popular_ticket_types': popular_ticket_types,
         'movies_with_reservations': movies_with_reservations,
     }
     return render(request, 'backoffice/report.html', context)
