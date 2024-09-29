b3c9471f48a0f5a5fe51de7a120ba3118c07fbde
Tomasz Brzezina <tomasz@brzezina.pl>
2024-09-29 19:24:38 +0200

    widoki

--- cinema_reservation_system/backoffice/views.py
+++ cinema_reservation_system/backoffice/views.py
@@ -1,4 +1,5 @@
 from django.db.models import Count
+from django.db.models.functions import ExtractHour, ExtractMinute
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -34,7 +35,17 @@ def html_report(request):
             total_reserved_seats=Count('seance__reservation__seatreservation')
         ).order_by('-total_reserved_seats')
     )
+    popular_showtimes = (
+        models.Seance.objects.annotate(hour=ExtractHour('show_start'), minute=ExtractMinute('show_start'))  # Ekstrahuje godzinę
+        .values('hour','minute')  # Grupuje po godzinie
+        .annotate(count=Count('id'))  # Zlicza wystąpienia seansów dla każdej godziny
+        .order_by('-count')  # Sortuje według liczby wystąpień malejąco
+    )
+
     context = {
+        'range_begin' : range_begin,
+        'range_end': range_end,
+        'popular_showtimes': popular_showtimes,
         'movies_with_reservations': movies_with_reservations,
     }
     return render(request, 'backoffice/report.html', context)
