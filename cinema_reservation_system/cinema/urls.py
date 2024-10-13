c7f199fec0c2af4ad0f02caedb088bac61b09eae
Tomasz Brzezina <tomasz@brzezina.pl>
2024-10-13 13:53:49 +0200

    menu changes

--- cinema_reservation_system/cinema/urls.py
+++ cinema_reservation_system/cinema/urls.py
@@ -5,9 +5,10 @@ from django.urls import path
 urlpatterns = [
     path("", views.index, name="index"),
     path("set_cinema", views.set_cinema, name="set_cinema"),
-    path("cennik/", views.price_list, name="price_list"),
-    path("repertuar/", views.repertoire, name="repertoire"),
-    path("koszyk/", views.basket, name="basket"),
+    path("ticket_types/", views.price_list, name="price_list"),
+    path("repertoire/", views.repertoire, name="repertoire"),
+    path("reservation/", views.reservation, name="reservation"),
+    path("basket/", views.basket, name="basket"),
     path('select_movie/', views.select_movie, name='select_movie'),
     path('select_seance/<int:movie_id>/', views.select_seance, name='select_seance'),
     path('select_seats/<int:seance_id>/', views.select_seats, name='select_seats'),
