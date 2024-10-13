4bda6bb2c03b7fa05b176f565c7cc4461997e419
Tomasz Brzezina <tomasz@brzezina.pl>
2024-10-13 14:47:46 +0200

    menu for all

--- cinema_reservation_system/cinema/urls.py
+++ cinema_reservation_system/cinema/urls.py
@@ -1,8 +1,11 @@
 from . import views
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path("", views.index, name="index"),
     path("set_cinema", views.set_cinema, name="set_cinema"),
     path("ticket_types/", views.price_list, name="price_list"),
