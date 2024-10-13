ab725d745d46e1f2855764757ecacb3a73fa55ff
Tomasz Brzezina <tomasz@brzezina.pl>
2024-10-14 01:00:24 +0200

    better

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -1,13 +1,18 @@
 #!/bin/sh -x
-cd ../cinema_reservation_system
+if [ -z "$VIRTUAL_ENV" ]; then
+    echo "Brak aktywnego środowiska wirtualnego. Zakończono działanie."
+    exit 1
+fi
+olddir=$(pwd)
+cd "$(dirname "$VIRTUAL_ENV")"/cinema_reservation_system
 pip install -r requirements.txt
 python manage.py migrate
-../tools/loaddata.sh
+sh $(dirname "$VIRTUAL_ENV")/tools/loaddata.sh
 echo "please wait - generating seances"
 python manage.py generate_seances
 echo "please wait - generating reservations"
 python manage.py generate_reservations
-cd -
+cd "$olddir"
 clear
 echo "end of upgrading. Thank you"
 echo "Press Enter, to continue ..."
