be232459d87c9fea3547c1819a9273cdcb52c9d3
Tomasz Brzezina <tomasz@brzezina.pl>
2024-11-11 19:09:08 +0100

    charts

--- cinema_reservation_system/backoffice/views.py
+++ cinema_reservation_system/backoffice/views.py
@@ -5,6 +5,8 @@ from django.db.models.functions import ExtractHour, ExtractMinute, ExtractWeekDa
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.template.response import TemplateResponse
+from backoffice.charts import movies_with_reservations_chart, best_hours_chart, best_days_of_week_chart, best_ticket_types_chart
+
 from cinema import models
 from backoffice import decorators
 
@@ -61,15 +63,19 @@ def pdf_report(request):
     return HttpResponse("Hello, world. You're at the backoffice index.")
 
 @login_required
-def user_panel(request):
+@decorators.set_vars
+def user_panel(request, context):
     return render(request, 'backoffice/user_panel.html', {
         'first_name': request.user.first_name,
         'last_name': request.user.last_name,
         'email': request.user.email
     })
 
+
+
 @login_required
-def report(request):
+@decorators.set_vars
+def report(request, context):
     #     Stworzenie statystyki pod:
     #
     # Najczęściej wybierany film przez klienta (raport tyd/mięs)
@@ -80,45 +86,6 @@ def report(request):
     range_end = today.add(days=1)
     range_begin = today.subtract(days=7)
 
-    movies_with_reservations = (
-        models.Movie.objects.filter(
-            seances__show_start__gte=range_begin,
-            seances__show_start__lte=range_end,
-        ).annotate(
-            total_reserved_seats=Count('seances__reservation__seatreservation')
-        ).order_by('-total_reserved_seats')
-    )
-    popular_showtimes = (
-        models.SeatReservation.objects.filter(
-            reservation__seance__show_start__gte=range_begin,
-            reservation__seance__show_start__lte=range_end,
-        ).annotate(
-            hour=ExtractHour('reservation__seance__show_start'),
-            minute=ExtractMinute('reservation__seance__show_start')
-        ).exclude(hour__isnull=True
-        ).values('hour','minute'  # Grupuje po godzinie
-        ).annotate(count=Count('id')  # Zlicza wystąpienia seansów dla każdej godziny
-        ).order_by('-count')  # Sortuje według liczby wystąpień malejąco
-    )
-    popular_weekdays = (
-        models.SeatReservation.objects.filter(
-            reservation__seance__show_start__gte=range_begin,
-            reservation__seance__show_start__lte=range_end,
-        ).annotate(
-            weekday=ExtractWeekDay('reservation__seance__show_start')  # Ekstrahuje godzinę
-        ).exclude(weekday__isnull=True
-        ).values('weekday'
-        ).annotate(count=Count('id')  # Zlicza wystąpienia seansów dla każdej godziny
-        ).order_by('-count')  # Sortuje według liczby wystąpień malejąco
-    )
-    popular_weekdays = [
-        {
-            'weekday': popular_weekday['weekday'],
-            'day_name': DAYS_OF_WEEK.get(popular_weekday['weekday'], "Nieznany dzień"),
-            'count': popular_weekday['count']
-        }
-        for popular_weekday in popular_weekdays
-    ]
 
     popular_ticket_types = (
         models.SeatReservation.objects.filter(
@@ -131,13 +98,13 @@ def report(request):
         ).order_by('-count')  # Sortuje według liczby wystąpień malejąco
     )
 
-    context = {
+    context.update({
         'range_begin' : range_begin,
         'range_end': range_end,
-        'popular_showtimes': popular_showtimes,
-        'popular_weekdays': popular_weekdays,
-        'popular_ticket_types': popular_ticket_types,
-        'movies_with_reservations': movies_with_reservations,
-    }
+        'best_hours_chart': best_hours_chart(context['selected_cinema'], range_begin, range_end),
+        'best_days_of_week_chart': best_days_of_week_chart(context['selected_cinema'], range_begin, range_end),
+        'best_ticket_types_chart': best_ticket_types_chart(context['selected_cinema'], range_begin, range_end),
+        'movies_with_reservations_chart': movies_with_reservations_chart(context['selected_cinema'], range_begin, range_end),
+    })
     return render(request, 'backoffice/report.html', context)
 
