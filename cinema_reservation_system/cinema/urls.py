487e0b2c34f92362c42c449b51bbce59387949c2
Tomasz Brzezina <tomasz@brzezina.pl>
2024-11-14 20:15:10 +0100

    rollback

--- cinema_reservation_system/cinema/urls.py
+++ cinema_reservation_system/cinema/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('tickets/', views.tickets, name='tickets'),
     path('payment/reservation/<int:reservation_id>/', views.payment, name='pay_one'),
     path('change_password/', views.change_password, name='change_password'),
-
+    path('qr_code/<int:reservation_id>/', views.qr_code_view, name='qr_code'),
 ]
 
